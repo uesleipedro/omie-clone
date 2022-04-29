0434bd796c255505e4db03670674d7ef113d82fb
uesleipedro <ielseu@hotmail.com>
2022-04-28 23:30:35 -0300

    create initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next'
+import CustomButton from '../compnents/CustomButton'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Omie Clone</h1>
+      <div className={styles.text}>
+        <div className={styles.textArea}>
+          <span className='title'>
+            Adquira o mais
+            completo sistema de
+            gestão empresarial
+            do mercado!
+          </span>
+          <span className={styles.subtitle}>Controle toda a sua empresa com um sistema 100% online.</span>
+          <CustomButton name='Fale com um especialista' color='green' />
+        </div>
+        <div className={styles.imageArea}></div>
+      </div>
     </div>
   )
 }
